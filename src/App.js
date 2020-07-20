c622b00a75c22b57cbced01d1660979dddbcba61
Alex Procter-Panzer <acoprocter@gmail.com>
2020-07-20 14:11:25 +1200

    create basic redux store

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import Sidebar from "./Sidebar.js";
 import Main from "./Main.js";
+import store from "./store";
 
 const App = () => {
+  const { contacts } = store.getState();
+  
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar contacts={contacts} />
       <Main />
     </div>
   );
