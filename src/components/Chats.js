16c6d31258f6ceeae5a377e6a3472e76f48664fd
Alex Procter-Panzer <acoprocter@gmail.com>
2020-07-20 19:01:12 +1200

    add message input functionality

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -9,15 +9,29 @@ const Chat = ({ message }) => {
 };
 
 class Chats extends Component {
-  render() {
-    return (
-      <div className="Chats">
-        {this.props.messages.map(message => (
-          <Chat message={message} key={message.number} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default Chats;
+    constructor(props) {
+        super(props);
+        this.chatsRef = React.createRef();
+      }
+      componentDidMount() {
+        this.scrollToBottom();
+      }
+      componentDidUpdate() {
+        this.scrollToBottom();
+      }
+      scrollToBottom = () => {
+        this.chatsRef.current.scrollTop = this.chatsRef.current.scrollHeight;
+      };
+    
+      render() {
+        return (
+          <div className="Chats" ref={this.chatsRef}>
+            {this.props.messages.map(message => (
+              <Chat message={message} key={message.number} />
+            ))}
+          </div>
+        );
+      }
+    }
+    
+    export default Chats;
--- src/containers/ChatWindow.js
+++ src/containers/ChatWindow.js
@@ -4,16 +4,23 @@ import Header from "../components/Header";
 import Chats from "../components/Chats"; 
 import _ from 'lodash';
 import "./ChatWindow.css";
+import MessageInput from "./MessageInput";
+
+
+
+
 
 const ChatWindow = ({ activeUserId }) => {
   const state = store.getState();
   const activeUser = state.contacts.contacts[activeUserId];
   const activeMsgs = state.messages[activeUserId];
-  
+  const { typing } = state;
+
   return (
     <div className="ChatWindow">
       <Header user={activeUser} />
       <Chats messages={_.values(activeMsgs)} />
+      <MessageInput value={typing} />
     </div>
   );
 };
